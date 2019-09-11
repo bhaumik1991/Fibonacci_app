dfbf8693ea4e798f7e6d866865a559c8c0eb14e9
commonsub <commonsub816@gmail.com>
2019-09-11 11:18:14 +0530

    issue fixed for supporting large numbers in result

--- Fibonacci/settings.py
+++ Fibonacci/settings.py
@@ -77,13 +77,10 @@ WSGI_APPLICATION = 'Fibonacci.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Fibonacci',
-        'USER':'root',
-        'PASSWORD':'newpassword'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
